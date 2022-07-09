d97ce234ea1a7d78748bcda2216353fac71657f9
Kislyak <flamewind2po100@gmail.com>
2022-07-09 19:36:55 +0300

    5 point complite

--- seminariVse/Dz/Dz.py
+++ seminariVse/Dz/Dz.py
@@ -81,16 +81,19 @@
     
 #     - A (3,6); B (2,1) -> 5,09
 #     - A (7,-5); B (1,-1) -> 7,21
-import math
-Ax = int(input('Введите X для первой точки: '))
-Ay = int(input('Введите y для первой точки: '))
-Bx = int(input('Введите X для второй точки: '))
-By = int(input('Введите y для второй точки: '))
-# first_coordinate = (Ax, Ay)
-# second_coordinate = (Bx, By)
+                                                                #-------Comlite
+# import math       
+Ax = int (input('Введите X для первой точки: '))
+Ay = int (input('Введите y для первой точки: '))
+Bx = int (input('Введите X для второй точки: '))
+By = int (input('Введите y для второй точки: '))
+first_coordinate = (Ax, Ay)
+second_coordinate = (Bx, By)
 # # print (first_coordinate)
 # # print (second_coordinate)
 # AB = √(xb - xa)^2 + (yb - ya)^2
-distance = math.sqrt((Bx - Ax) ** 2 + (By - Ay) ** 2)
-# distance =((Bx - Ax) ** 2 + (By - Ay) ** 2)**0.5
-print (distance)
+# distance = math.sqrt((Bx - Ax) ** 2 + (By - Ay) ** 2)
+distance =((Bx - Ax) ** 2 + (By - Ay) ** 2)**0.5
+print ('для: ', 'A', first_coordinate, 'B', second_coordinate, 'Расстояние между точками --> ', distance)
+
+#пока самые очевидные решения применил)
