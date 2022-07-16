c09b4c9bcbf0ec5fab6064e460a04da979e96bb9
Kislyak <flamewind2po100@gmail.com>
2022-07-16 15:31:00 +0300

    ewe 4ut dopisal

--- seminariVse/Dz/Dz2/exercise1.py
+++ seminariVse/Dz/Dz2/exercise1.py
@@ -4,3 +4,25 @@
 # *Пример:*
 # - 6782 -> 23
 # - 0,56 -> 11
+
+num = str(input('Введите вещественное число:')) 
+# def is_number(num): #проверка Try-Except(если строка содержит отрицательное число и число с плавающей точкой )
+#     num = str(input('Введите вещественное число:')) 
+#     try:
+#         float(num)
+#         return True
+#     except ValueError:
+#         return False # если строка кроме цифр,знака минуса и точки содержит другие символы 
+x = num.split('.')
+res_x = str(num).replace('-', '') # убираем минус 
+print(res_x)
+part1 = int(x[0])
+part2 = int(x[1])
+sum = 0
+while (part1 != 0):
+    sum = sum + (part1 % 10)
+    part1 = part1 // 10
+while (part2 != 0):
+    sum = sum + (part2 % 10)
+    part2 = part2 // 10
+print(f'Резултат: {sum}')
