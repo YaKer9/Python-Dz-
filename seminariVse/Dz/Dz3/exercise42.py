120777ab0b623777e6476ebc79e70a1799092f1f
Kislyak <flamewind2po100@gmail.com>
2022-07-16 23:37:50 +0300

    mdauj

--- seminariVse/Dz/Dz3/exercise42.py
+++ seminariVse/Dz/Dz3/exercise42.py
@@ -12,13 +12,15 @@ print(num_list)
 string_num=[]
 for element in num_list:                 #тут мы переводим строку из ['2', '4'] В лист [2, 4]
     string_num.append(int(element))
-print(string_num)
-rev_string_num = string_num[::-1]
-print(rev_string_num)
+print(string_num)                        #выводим лист какой нам нужен
+# rev_string_num = string_num[::-1]      #тут я создавал перевернутую строку, но вроде она ненужна оказалась
+# print(rev_string_num)
 sum_different_sides= []  
-# for i in range(0,len(string_num)):
-#     sum_different_sides.append(string_num[i]*rev_string_num[i])
-# for i in range(0, len(string_num)):
-#     sum_different_sides.append(string_num[i]*string_num[-1*i])
 
-# print(sum_different_sides)
+for i in range(1, len(string_num)):      
+    sum_different_sides.append(string_num[(i-1)]*string_num[(-1*i)])
+print(sum_different_sides)               #тут перемножаю поиндексно с 1го по последний элементы с -1 по -последний
+                                         #двигаюсь с краев к другим краям и друг на друга перемножаю(а надо как то к центру =(
+                                         # а то они начинают дублироваться)
+
+
