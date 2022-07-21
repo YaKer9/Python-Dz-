e788ed93a334ee38a12fa90954b1e2eae4488af8
Kislyak <flamewind2po100@gmail.com>
2022-07-21 21:48:08 +0300

    EEEEEEEEEEEEEE

--- seminariVse/seminar1/semi4.py
+++ seminariVse/seminar1/semi4.py
@@ -92,32 +92,32 @@
 
 ##########################
 
-from random import randint
+# from random import randint
 
 
-def input_number_test_float(text):
-    int_test = True
-    is_minus = False
-    while int_test:
-def list_length_n(n:int = 0,lst: list = []):
-    for i in range(-n,n):
-        lst.append(randint(n*-1,n+1))
-    print(lst)
-    return lst
-lst = list_length_n(n,)
+# def input_number_test_float(text):
+#     int_test = True
+#     is_minus = False
+#     while int_test:
+# def list_length_n(n:int = 0,lst: list = []):
+#     for i in range(-n,n):
+#         lst.append(randint(n*-1,n+1))
+#     print(lst)
+#     return lst
+# lst = list_length_n(n,)
 
-with open('text.txt', mode = 'w') as file:
-    for k in range(0,5):
-        file.write(f"\n{randint(0, len(lst)-1)}")
+# with open('text.txt', mode = 'w') as file:
+#     for k in range(0,5):
+#         file.write(f"\n{randint(0, len(lst)-1)}")
     
-with open('text.txt', mode = 'r') as file:
-    res = 1
-    lst_index = []
-    for line in file.read():
-        if line.isdigit():
-           res *= lst[int(line)] 
-           lst_index.append(line)
-print(f"{lst_index=}")           
-print(f"{lst=}")
-print(res)
-#############
+# with open('text.txt', mode = 'r') as file:
+#     res = 1
+#     lst_index = []
+#     for line in file.read():
+#         if line.isdigit():
+#            res *= lst[int(line)] 
+#            lst_index.append(line)
+# print(f"{lst_index=}")           
+# print(f"{lst=}")
+# print(res)
+# #############
