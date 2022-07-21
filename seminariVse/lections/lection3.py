6ae1605a55a55a086b71437e3d45dc9f6df1e994
Kislyak <flamewind2po100@gmail.com>
2022-07-21 18:51:40 +0300

    update razbirayu

---
+++ seminariVse/lections/lection3.py
@@ -0,0 +1,71 @@
+
+# def f(x):
+#     return x**2
+
+# g = f
+
+# # print(f(4))
+# # print(g(4))
+
+# def calc1(x):
+#     return x+10
+
+# def calc2(x):
+#     return x*10
+# # print(calc2(10))
+
+# def math(op, x):
+#     print (op(x))
+
+# math(calc2,10)
+# math(calc1,10)
+
+#################
+
+# def sum(x,y):
+#     return x+y
+
+# sum = lambda x, y: x + y + 1
+
+# def mylt(x,y):
+#     return x*y
+
+# def calc(op, a, b):
+#     print(op(a, b))
+#     # return (a, b)
+
+# calc(lambda x, y: x + y, 4, 5)
+
+###########
+
+# List Comprehension
+# [exp for item in iterable]
+
+# [exp for item in iterable (if conditional)]
+
+# [exp <if conditional> for item in iterable (if conditional)] - 4toto strawnoe
+
+# list = []
+# for i in range(1, 100):
+#     # if(i%2==0):
+#     list.append(i)
+
+# print(list)
+
+
+# def f(x):
+#     return x**3
+
+#         #vskobkah nije kortej
+# list = [(i, f(i)) for i in range(1, 21) if i % 2 == 0]
+# print(list)
+
+#################
+
+# a = [1, 2, 4, 7, 22, 13, 21, 6]
+
+# c = []
+# for i in a:
+#     if i % 2 == 0:
+#         c.append(i)
+
